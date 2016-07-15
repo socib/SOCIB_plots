daf4cd545046121e77efe3c03d47ec300916304d
Charles Troupin <ctroupin@socib.es>
2016-07-15 16:54:52 +0200

    added loading function

--- Missions/GlidingTurtles/load_functions.py
+++ Missions/GlidingTurtles/load_functions.py
@@ -2,7 +2,7 @@
 
 import os
 import numpy as np
-import netCDF4 as netcdf
+import netCDF4 
 import cf
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -14,7 +14,7 @@ from matplotlib._png import read_png
 
 
 def load_glider_position(filename):
-    with netcdf.Dataset(filename) as nc:
+    with netcdf.netCDF4(filename) as nc:
         lon = nc.variables['longitude'][:]
         lat = nc.variables['latitude'][:]
     return lon, lat
@@ -28,7 +28,7 @@ def load_sst_modis(filename):
         var2load = 'sst'
         varqc2load = 'qual_sst'
 
-    with netcdf.Dataset(filename) as nc:
+    with netcdf.netCDF4(filename) as nc:
         lon = nc.groups['navigation_data'].variables['longitude'][:]
         lat = nc.groups['navigation_data'].variables['latitude'][:]
         sst = nc.groups['geophysical_data'].variables[var2load][:]
@@ -37,3 +37,21 @@ def load_sst_modis(filename):
         # apply QC filter
         sst = np.ma.masked_where(qualsst > 1, sst)
     return lon, lat, sst
+
+def load_altimetry_aviso(altimetryfile, coordinates):
+    with netcdf.netCDF4(altimetryfile) as nc:
+        lon = nc.variables['lon'][:] - 360.
+        lat = nc.variables['lat'][:]
+        u = np.squeeze(nc.variables['u'][:])
+        v = np.squeeze(nc.variables['v'][:])
+        # subset
+        goodlon = np.where(np.logical_and((lon >= coordinates[0]), (lon <= coordinates[1])))[0]
+        goodlat = np.where(np.logical_and((lat >= coordinates[2]), (lat <= coordinates[3])))[0]
+        lon = lon[goodlon]
+        lat = lat[goodlat]
+        u = u[goodlat, :]
+        u = u[:, goodlon]
+        v = v[goodlat, :]
+        v = v[:, goodlon]
+    return lon, lat, u, v
+
